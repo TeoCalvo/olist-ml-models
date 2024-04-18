d2d2efc2890bcda6fa49b29602fc3f0ce5cbc3e3
pimenta388@gmail.com <pimenta388@gmail.com>
2024-04-18 13:14:30 +0000

    Variáveis de Entrega

---
+++ SRC_Novo(MEu)/ETL/Varaiveis_de_Entrega.sql
@@ -0,0 +1,46 @@
+-- Databricks notebook source
+WITH tb_pedido AS(
+
+  SELECT t1.idPedido,
+        t2.idVendedor,
+        t1.descSituacao,
+        t1.dtPedido,
+        t1.dtAprovado,
+        t1.dtEntregue,
+        t1.dtEstimativaEntrega,
+        sum(t2.vlFrete) as TotalFrete
+
+  FROM silver.olist.pedido as t1
+
+  LEFT JOIN silver.olist.item_pedido as t2
+  ON t1.idPedido = t2.idPedido
+
+  WHERE dtPedido < '2018-01-01'
+  AND dtPedido >= add_months('2018-01-01', -6)
+
+  GROUP BY t1.idPedido,
+        t2.idVendedor,
+        t1.descSituacao,
+        t1.dtPedido,
+        t1.dtAprovado,
+        t1.dtEntregue,
+        t1.dtEstimativaEntrega
+)
+
+SELECT  idVendedor,
+        COUNT(DISTINCT CASE WHEN DATE(coalesce(dtEntregue,'2018-01-01') ) > DATE(dtEstimativaEntrega) THEN idPedido END)  / 
+        COUNT (DISTINCT CASE WHEN descSituacao = 'delivered' AND DATE(dtEntregue) > DATE(dtEstimativaEntrega) THEN idPedido END) AS pctPedidoAtraso,
+        COUNT(DISTINCT CASE WHEN descSituacao = 'canceled' THEN idPedido END) /  COUNT(DISTINCT idPedido) AS pctPedidoCancelado,
+        avg(TotalFrete) as avgFrete,
+        percentile(TotalFrete,0.5) as medianFrete,
+        max(TotalFrete) as maxFrete,
+        min(TotalFrete) as minFrete,
+        avg(date_diff(coalesce(dtEntregue, '2018-01-01'),dtAprovado)) as qtdDiasAprovadoEntrega,
+        avg(date_diff(coalesce(dtEntregue, '2018-01-01'),dtPedido)) as qtdDiasPedidoEntrega,
+        avg(date_diff(dtEstimativaEntrega, coalesce(dtEntregue, '2018-01-01'))) AS qtdDiasEntregaPromessa
+        
+
+
+FROM tb_pedido
+
+GROUP BY 1
